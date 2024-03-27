099c772fd3f5a082d2097d98b0eacbf1ee601334
nik-kita <nik-kita.deno.dev>
2024-03-27 06:56:15 +0100

    ...

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import "./style.css";
 import App from "./App.vue";
 import VueGoogleSignin from "vue3-google-signin";
 
+console.log(import.meta.env)
+
 createApp(App)
   .use(VueGoogleSignin, {
     clientId: import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID,
